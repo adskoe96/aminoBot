20979e4cbf1fdea7e00d9cdbd802c8def35241bc
adskoe96 <73867345+adskoe96@users.noreply.github.com>
2020-11-28 16:50:58 +0500

    Update botRelease.py

--- botRelease.py
+++ botRelease.py
@@ -51,9 +51,9 @@ async def on_message(message):
 			b = uzer.aminoId
 			n = uzer.accountMembershipStatus
 			if n == 1:
-				n = 'Amino+ в наличии'
+				n = '✔️'
 			elif n == 0:
-				n = 'Amino+ отсутствует'
+				n = '❌'
 			else:
 				pass
 			if v == 1:
